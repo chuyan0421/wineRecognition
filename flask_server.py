914e1cd49459b6d225af10590da3c62938ffec4e
chuyan0421 <chuyan0421@126.com>
2018-12-05 21:34:17 +0800

    20181205d

--- flask_server.py
+++ flask_server.py
@@ -5,6 +5,7 @@ from PIL import Image
 import numpy as np
 import flask
 import io
+import tensorflow as tf
 
 # initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
@@ -16,6 +17,8 @@ def load_model():
     # substitute in your own networks just as easily)
     global model
     model = ResNet50(weights="imagenet")
+    global graph
+    graph = tf.get_default_graph()
 
 def prepare_image(image, target):
     # if the image mode is not RGB, convert it
@@ -50,18 +53,19 @@ def predict():
 
             # classify the input image and then initialize the list
             # of predictions to return to the client
-            preds = model.predict(image)
-            results = imagenet_utils.decode_predictions(preds)
-            data["predictions"] = []
+            with graph.as_default():
+                preds = model.predict(image)
+                results = imagenet_utils.decode_predictions(preds)
+                data["predictions"] = []
 
-            # loop over the results and add them to the list of
-            # returned predictions
-            for (imagenetID, label, prob) in results[0]:
-                r = {"label": label, "probability": float(prob)}
-                data["predictions"].append(r)
+                # loop over the results and add them to the list of
+                # returned predictions
+                for (imagenetID, label, prob) in results[0]:
+                    r = {"label": label, "probability": float(prob)}
+                    data["predictions"].append(r)
 
-            # indicate that the request was a success
-            data["success"] = True
+                # indicate that the request was a success
+                data["success"] = True
 
     # return the data dictionary as a JSON response
     return flask.jsonify(data)
