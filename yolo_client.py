b5fd6705ee35afea127e057964c1cee851470eec
chuyan0421 <chuyan0421@126.com>
2018-12-05 16:38:27 +0800

    20181205

---
+++ yolo_client.py
@@ -0,0 +1,54 @@
+from grpc.beta import implementations
+import tensorflow as tf
+
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2_grpc
+from yolo3.utils import letterbox_image
+from PIL import Image
+import numpy as np
+import grpc
+
+hostport = '192.168.33.55:8500'
+
+img_path = '5.jpg'
+
+def image_precess(img_path):
+
+    image = Image.open(img_path)
+    boxed_image = letterbox_image(image, tuple(reversed((416, 416))))
+
+    image_data = np.array(boxed_image, dtype='float32')
+    image_data /= 255.
+    image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+
+    return image_data
+
+
+def _create_rpc_callback():
+    def _callback(result_future):
+        exception = result_future.exception()
+        if exception:
+            print(exception)
+        else:
+            print(result_future.result().outputs['outputs'])
+
+    return _callback
+
+
+if __name__ == '__main__':
+
+    channel = grpc.insecure_channel(hostport)
+    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+
+    data = image_precess(img_path)
+
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'yolo'
+    request.model_spec.signature_name = 'classify'
+    request.inputs['input'].CopyFrom(
+        tf.contrib.util.make_tensor_proto(data, shape=None)
+    )
+    result_future = stub.Predict(request, 5.0) # 10 secs timeout
+    print(result_future)
+    # result_future.add_done_callback(_create_rpc_callback())
+
--- yolo_server.py
+++ yolo_server.py
@@ -1,76 +1,98 @@
-from keras.applications import ResNet50
-from keras.preprocessing.image import img_to_array
-from keras.applications import imagenet_utils
-from PIL import Image
+import tensorflow as tf
+from keras import backend as K
+from keras.models import Sequential, Model
+from os.path import isfile
+from keras.models import load_model
+import argparse
+from yolo3.model import yolo_eval, yolo_body, tiny_yolo_body
+from keras.layers import Input
+import os
 import numpy as np
-import flask
-import io
-
-# initialize our Flask application and the Keras model
-app = flask.Flask(__name__)
-model = None
-
-def load_model():
-    # load the pre-trained Keras model (here we are using a model
-    # pre-trained on ImageNet and provided by Keras, but you can
-    # substitute in your own networks just as easily)
-    global model
-    model = ResNet50(weights="imagenet")
-
-def prepare_image(image, target):
-    # if the image mode is not RGB, convert it
-    if image.mode != "RGB":
-        image = image.convert("RGB")
-
-    # resize the input image and preprocess it
-    image = image.resize(target)
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-    image = imagenet_utils.preprocess_input(image)
-
-    # return the processed image
-    return image
 
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    # initialize the data dictionary that will be returned from the
-    # view
-    data = {"success": False}
-
-    # ensure an image was properly uploaded to our endpoint
-    if flask.request.method == "POST":
-        if flask.request.files.get("image"):
-            # read the image in PIL format
-            image = flask.request.files["image"].read()
-            image = Image.open(io.BytesIO(image))
-
-            # preprocess the image and prepare it for classification
-            image = prepare_image(image, target=(224, 224))
-
-            # classify the input image and then initialize the list
-            # of predictions to return to the client
-            preds = model.predict(image)
-            results = imagenet_utils.decode_predictions(preds)
-            data["predictions"] = []
-
-            # loop over the results and add them to the list of
-            # returned predictions
-            for (imagenetID, label, prob) in results[0]:
-                r = {"label": label, "probability": float(prob)}
-                data["predictions"].append(r)
-
-            # indicate that the request was a success
-            data["success"] = True
-
-    # return the data dictionary as a JSON response
-    return flask.jsonify(data)
+def get_class():
+    classes_path = os.path.expanduser(FLAGS.classes_path)
+    with open(classes_path) as f:
+        class_names = f.readlines()
+    class_names = [c.strip() for c in class_names]
+    return class_names
 
 
-# if this is the main thread of execution first load the model and
-# then start the server
+def get_anchors():
+    anchors_path = os.path.expanduser(FLAGS.anchors_path)
+    with open(anchors_path) as f:
+        anchors = f.readline()
+    anchors = [float(x) for x in anchors.split(',')]
+    return np.array(anchors).reshape(-1, 2)
+
+
+def build_model():
+
+    anchors = get_anchors()
+    class_names = get_class()
+
+    num_anchors = len(anchors)
+    num_classes = len(class_names)
+
+    model = yolo_body(Input(shape=(None, None, 3)), num_anchors // 3, num_classes)
+    model.load_weights(FLAGS.model_path)
+    print('{} model, anchors, and classes loaded.'.format(FLAGS.model_path))
+    return model
+
+
+def save_model_to_serving(model, export_version, export_path):
+    print(model.input, model.output)
+    signature = tf.saved_model.signature_def_utils.predict_signature_def(
+        inputs={'input': model.input}, outputs={'outputs': tf.convert_to_tensor(model.output)}
+    )
+    exported_path = os.path.join(
+        tf.compat.as_bytes(export_path),
+        tf.compat.as_bytes(str(export_version))
+    )
+    builder = tf.saved_model.builder.SavedModelBuilder(exported_path)
+    legacy_init_op = tf.group(tf.tables_initializer(), name='legacy_init_op')
+    builder.add_meta_graph_and_variables(
+        sess= K.get_session(),
+        tags=[tf.saved_model.tag_constants.SERVING],
+        signature_def_map={
+            'classify': signature,
+        },
+        legacy_init_op=legacy_init_op
+    )
+    builder.save()
+
+
 if __name__ == '__main__':
-    print(("* Loading Keras model and Flask starting server..."
-        "please wait until server has fully started"))
-    load_model()
-    app.run()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--model_path', type=str, default='model_data/yolo_weights.h5',
+        help='path to model weight file'
+    )
+
+    parser.add_argument(
+        '--anchors_path', type=str, default='model_data/yolo_anchors.txt',
+        help='path to anchors file'
+    )
+
+    parser.add_argument(
+        '--classes_path', type=str, default='model_data/coco_classes.txt',
+        help='path to coco classed file'
+    )
+
+    parser.add_argument(
+        '--yolo_saved_model', type=str, default='/tmp/yolo_saved_model',
+        help='path to coco classed file'
+    )
+
+
+    FLAGS, unparsed = parser.parse_known_args()
+    yolo_model = build_model()
+
+    export_path = FLAGS.yolo_saved_model
+    if not os.path.exists(export_path):
+        os.mkdir(export_path)
+    save_model_to_serving(yolo_model, '1', export_path)
+
+
+
