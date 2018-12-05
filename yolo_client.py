4939de99a526c5ff6ba7171b43bdbfbd731f5be3
chuyan0421 <chuyan0421@126.com>
2018-12-05 18:32:59 +0800

    20181205c

--- yolo_client.py
+++ yolo_client.py
@@ -12,14 +12,14 @@ hostport = '192.168.33.55:8500'
 
 img_path = '5.jpg'
 
-def image_precess(img_path):
 
-    image = Image.open(img_path)
+def image_precess(image):
     boxed_image = letterbox_image(image, tuple(reversed((416, 416))))
 
     image_data = np.array(boxed_image, dtype='float32')
-    image_data /= 255.
+    image_data = np.multiply(image_data, 1.0 / 255.0)
     image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+    # image_data = image_data.astype(np.float32)
 
     return image_data
 
@@ -40,13 +40,14 @@ if __name__ == '__main__':
     channel = grpc.insecure_channel(hostport)
     stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
 
-    data = image_precess(img_path)
+    image = Image.open(img_path)
+    data = image_precess(image)
 
     request = predict_pb2.PredictRequest()
     request.model_spec.name = 'yolo'
     request.model_spec.signature_name = 'classify'
     request.inputs['input'].CopyFrom(
-        tf.contrib.util.make_tensor_proto(data, shape=None)
+        tf.contrib.util.make_tensor_proto(data)
     )
     result_future = stub.Predict(request, 5.0) # 10 secs timeout
     print(result_future)
